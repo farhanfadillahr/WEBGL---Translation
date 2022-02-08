27568c491025e75e35e82d558cb79240c18987fe
farhanfadillahr <65506753+farhanfadillahr@users.noreply.github.com>
2022-02-08 15:32:04 +0700

    Add files via upload

---
+++ translation/translation.js
@@ -0,0 +1,305 @@
+"use strict";
+
+function error(msg) {
+    if (topWindow.console) {
+        if (topWindow.console.error) {
+            topWindow.console.error(msg);
+        } else if (topWindow.console.log) {
+            topWindow.console.log(msg);
+        }
+    }
+}
+
+function loadShader(gl, shaderSource, shaderType, opt_errorCallback) {
+    const errFn = opt_errorCallback || error;
+    // Create the shader object
+    const shader = gl.createShader(shaderType);
+
+    // Load the shader source
+    gl.shaderSource(shader, shaderSource);
+
+    // Compile the shader
+    gl.compileShader(shader);
+
+    // Check the compile status
+    const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!compiled) {
+        // Something went wrong during compilation; get the error
+        const lastError = gl.getShaderInfoLog(shader);
+        errFn('*** Error compiling shader \'' + shader + '\':' + lastError + `\n` + shaderSource.split('\n').map((l, i) => `${i + 1}: ${l}`).join('\n'));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+function createShaderFromScript(
+    gl, scriptId, opt_shaderType, opt_errorCallback) {
+    let shaderSource = '';
+    let shaderType;
+    const shaderScript = document.getElementById(scriptId);
+    if (!shaderScript) {
+        throw ('*** Error: unknown script element' + scriptId);
+    }
+    shaderSource = shaderScript.text;
+
+    if (!opt_shaderType) {
+        if (shaderScript.type === 'x-shader/x-vertex') {
+            shaderType = gl.VERTEX_SHADER;
+        } else if (shaderScript.type === 'x-shader/x-fragment') {
+            shaderType = gl.FRAGMENT_SHADER;
+        } else if (shaderType !== gl.VERTEX_SHADER && shaderType !== gl.FRAGMENT_SHADER) {
+            throw ('*** Error: unknown shader type');
+        }
+    }
+
+    return loadShader(
+        gl, shaderSource, opt_shaderType ? opt_shaderType : shaderType,
+        opt_errorCallback);
+}
+
+function createProgram(
+    gl, shaders, opt_attribs, opt_locations, opt_errorCallback) {
+    const errFn = opt_errorCallback || error;
+    const program = gl.createProgram();
+    shaders.forEach(function (shader) {
+        gl.attachShader(program, shader);
+    });
+    if (opt_attribs) {
+        opt_attribs.forEach(function (attrib, ndx) {
+            gl.bindAttribLocation(
+                program,
+                opt_locations ? opt_locations[ndx] : ndx,
+                attrib);
+        });
+    }
+    gl.linkProgram(program);
+
+    // Check the link status
+    const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!linked) {
+        // something went wrong with the link
+        const lastError = gl.getProgramInfoLog(program);
+        errFn('Error in program linking:' + lastError);
+
+        gl.deleteProgram(program);
+        return null;
+    }
+    return program;
+}
+
+
+const defaultShaderType = [
+    'VERTEX_SHADER',
+    'FRAGMENT_SHADER',
+];
+
+function createProgramFromScripts(
+    gl, shaderScriptIds, opt_attribs, opt_locations, opt_errorCallback) {
+    const shaders = [];
+    for (let ii = 0; ii < shaderScriptIds.length; ++ii) {
+        shaders.push(createShaderFromScript(
+            gl, shaderScriptIds[ii], gl[defaultShaderType[ii]], opt_errorCallback));
+    }
+    return createProgram(gl, shaders, opt_attribs, opt_locations, opt_errorCallback);
+}
+
+function resizeCanvasToDisplaySize(canvas, multiplier) {
+    multiplier = multiplier || 1;
+    const width = canvas.clientWidth * multiplier | 0;
+    const height = canvas.clientHeight * multiplier | 0;
+    if (canvas.width !== width || canvas.height !== height) {
+        canvas.width = width;
+        canvas.height = height;
+        return true;
+    }
+    return false;
+}
+
+// const gopt = getQueryParams();
+
+function setupSlider(selector, options) {
+    var parent = document.querySelector(selector);
+    if (!parent) {
+        // like jquery don't fail on a bad selector
+        return;
+    }
+    if (!options.name) {
+        options.name = selector.substring(1);
+    }
+    return createSlider(parent, options); // eslint-disable-line
+}
+
+
+function createSlider(parent, options) {
+    var precision = options.precision || 0;
+    var min = options.min || 0;
+    var step = options.step || 1;
+    var value = options.value || 0;
+    var max = options.max || 1;
+    var fn = options.slide;
+    var name = options.name;
+    var uiPrecision = options.uiPrecision === undefined ? precision : options.uiPrecision;
+    var uiMult = options.uiMult || 1;
+
+    min /= step;
+    max /= step;
+    value /= step;
+
+    parent.innerHTML = `
+      <div class="gman-widget-outer">
+        <div class="gman-widget-label">${name}</div>
+        <div class="gman-widget-value"></div>
+        <input class="gman-widget-slider" type="range" min="${min}" max="${max}" value="${value}" />
+      </div>
+    `;
+    var valueElem = parent.querySelector(".gman-widget-value");
+    var sliderElem = parent.querySelector(".gman-widget-slider");
+
+    function updateValue(value) {
+        valueElem.textContent = (value * step * uiMult).toFixed(uiPrecision);
+    }
+
+    updateValue(value);
+
+    function handleChange(event) {
+        var value = parseInt(event.target.value);
+        updateValue(value);
+        fn(event, { value: value * step });
+    }
+
+    sliderElem.addEventListener('input', handleChange);
+    sliderElem.addEventListener('change', handleChange);
+
+    return {
+        elem: parent,
+        updateValue: (v) => {
+            v /= step;
+            sliderElem.value = v;
+            updateValue(v);
+        },
+    };
+}
+
+function main() {
+    // Get A WebGL context
+    /** @type {HTMLCanvasElement} */
+    var canvas = document.querySelector("#canvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // setup GLSL program
+    var program = createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
+
+    // look up where the vertex data needs to go.
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+
+    // lookup uniforms
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    var colorLocation = gl.getUniformLocation(program, "u_color");
+    var translationLocation = gl.getUniformLocation(program, "u_translation");
+
+    // Create a buffer to put positions in
+    var positionBuffer = gl.createBuffer();
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // Put geometry data into buffer
+    setGeometry(gl);
+
+    var translation = [100, 150];
+    var color = [Math.random(), Math.random(), Math.random(), 1];
+
+    drawScene();
+
+    // Setup a ui.
+    setupSlider("#x", { value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+    setupSlider("#y", { value: translation[1], slide: updatePosition(1), max: gl.canvas.height });
+
+    function updatePosition(index) {
+        return function (event, ui) {
+            translation[index] = ui.value;
+            drawScene();
+        };
+    }
+
+    // Draw the scene.
+    function drawScene() {
+        resizeCanvasToDisplaySize(gl.canvas);
+
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // Clear the canvas.
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+
+        // Turn on the attribute
+        gl.enableVertexAttribArray(positionLocation);
+
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 2;          // 2 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            positionLocation, size, type, normalize, stride, offset);
+
+        // set the resolution
+        gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+        // set the color
+        gl.uniform4fv(colorLocation, color);
+
+        // Set the translation.
+        gl.uniform2fv(translationLocation, translation);
+
+        // Draw the geometry.
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 18;  // 6 triangles in the 'F', 3 points per triangle
+        gl.drawArrays(primitiveType, offset, count);
+    }
+}
+
+// Fill the buffer with the values that define a letter 'F'.
+function setGeometry(gl) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            // left column
+            0, 0,
+            30, 0,
+            0, 150,
+            0, 150,
+            30, 0,
+            30, 150,
+
+            // top rung
+            30, 0,
+            100, 0,
+            30, 30,
+            30, 30,
+            100, 0,
+            100, 30,
+
+            // middle rung
+            30, 60,
+            67, 60,
+            30, 90,
+            30, 90,
+            67, 60,
+            67, 90,
+        ]),
+        gl.STATIC_DRAW);
+}
+
+main();
